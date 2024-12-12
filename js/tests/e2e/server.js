67649d4fa83c3e71eea86bf7f2eab55fedba9fad
Michele Lo Russo <michele.lorusso@canonical.com>
2024-12-12 14:20:02 +0100

    fix(e2e): add rate limiter to reduce vulnerability to DoS attacks

--- js/tests/e2e/server.js
+++ js/tests/e2e/server.js
@@ -32,6 +32,17 @@ const {
 } = require("./fixtures/constants.cjs");
 
 const app = express();
+
+// set up rate limiter: maximum of five requests per minute
+var RateLimit = require("express-rate-limit");
+var limiter = RateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // max 100 requests per windowMs
+});
+
+// apply rate limiter to all requests
+app.use(limiter);
+
 app.listen(SERVER_PORT);
 
 const amsAgent = new https.Agent({
