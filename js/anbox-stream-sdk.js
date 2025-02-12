6de8432a24415bdd0155046bd9d88c950a3044df
Anusha Chaturvedi <anusha.chaturvedi@canonical.com>
2025-02-12 17:55:42 +0530

    fix: TICS inexistent property

--- js/anbox-stream-sdk.js
+++ js/anbox-stream-sdk.js
@@ -2825,16 +2825,14 @@ class AnboxWebRTCManager {
     this._controlChan.onerror = (err) => {
       if (this._controlChan !== null) {
         let code = ANBOX_STREAM_SDK_ERROR_WEBRTC_CONTROL_FAILED;
-        if (err.error) {
-          switch (err.error.sctpCauseCode) {
-            case SCP_CAUSE_CODE_USER_INITIATED_ABORT:
-              code = ANBOX_STREAM_SDK_ERROR_WEBRTC_DISCONNECTED;
-              break;
-            default:
-              break;
-          }
-          this._onError(`error on control channel: ${err.error.message}`, code);
+        switch (err?.error?.sctpCauseCode) {
+          case SCP_CAUSE_CODE_USER_INITIATED_ABORT:
+            code = ANBOX_STREAM_SDK_ERROR_WEBRTC_DISCONNECTED;
+            break;
+          default:
+            break;
         }
+        this._onError(`error on control channel: ${err?.error?.message}`, code);
       }
     };
     this._controlChan.onclose = () => this._log("control channel is closed");
