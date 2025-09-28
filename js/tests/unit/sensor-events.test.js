c8d8702502aa9df930655b77cb12afc327ebceb8
gary-wzl77 <gary.wang@canonical.com>
2025-09-28 11:25:51 +0800

    feat(js): add AnboxSensorManager class for sensor data delivery
    Add AnboxSensorManager class to capture and process device sensor data
    through HTML5 DeviceOrientation and DeviceMotion APIs, and send
    it to the Anbox container to influence the Android application which
    relies on the sensor data to function.

---
+++ js/tests/unit/sensor-events.test.js
@@ -0,0 +1,349 @@
+/*
+ * This file is part of Anbox Cloud Streaming SDK
+ *
+ * Copyright 2025 Canonical Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { AnboxStream } from "./anbox-stream-sdk";
+
+// Mock DeviceOrientationEvent
+if (!global.DeviceOrientationEvent) {
+  class DeviceOrientationEvent extends Event {
+    constructor(type, params = {}) {
+      super(type, params);
+      this.alpha = params.alpha ?? 0.0;
+      this.beta = params.beta ?? 0.0;
+      this.gamma = params.gamma ?? 0.0;
+    }
+  }
+  global.DeviceOrientationEvent = DeviceOrientationEvent;
+}
+
+// Mock DeviceMotionEvent
+if (!global.DeviceMotionEvent) {
+  class DeviceMotionEvent extends Event {
+    constructor(type, params = {}) {
+      super(type, params);
+      this.acceleration = params.acceleration ?? { x: 0.0, y: 0.0, z: 0.0 };
+      this.accelerationIncludingGravity =
+        params.accelerationIncludingGravity ?? { x: 0.0, y: 0.0, z: 0.0 };
+      this.rotationRate = params.rotationRate ?? {
+        alpha: 0.0,
+        beta: 0.0,
+        gamma: 0.0,
+      };
+    }
+  }
+  global.DeviceMotionEvent = DeviceMotionEvent;
+}
+
+const sdkOptions = {};
+function setupStream(sdkOptions) {
+  let stream = new AnboxStream(sdkOptions);
+
+  const video = document.createElement("video");
+  video.id = stream._videoID;
+  const audio = document.createElement("audio");
+  audio.id = stream._audioID;
+
+  let container = document.getElementById("foobar");
+  container.appendChild(video);
+  container.appendChild(audio);
+
+  const snapshots = [];
+  let mockFn = jest.fn((type, data) => {
+    snapshots.push({
+      type,
+      data: JSON.parse(JSON.stringify(data)),
+    });
+    return true;
+  });
+  stream._webrtcManager.sendControlMessage = mockFn;
+
+  return { stream, snapshots };
+}
+
+beforeEach(() => {
+  const container = document.createElement("div");
+  container.__defineGetter__("clientWidth", () => 2000);
+  container.__defineGetter__("clientHeight", () => 2000);
+  container.id = "foobar";
+  document.body.appendChild(container);
+  sdkOptions.connector = { connect() {}, disconnect() {} };
+  sdkOptions.targetElement = "foobar";
+  sdkOptions.controls = { emulateTouch: false };
+  global.navigator.__defineGetter__("maxTouchPoints", () => 5);
+  global.navigator.__defineGetter__(
+    "userAgent",
+    () =>
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.109 Safari/537.36",
+  );
+});
+
+test("should throw error when updateInterval option is less than 10ms", () => {
+  sdkOptions.devices = {
+    sensor: {
+      updateInterval: 5,
+      enableOrientation: true,
+    },
+  };
+  expect(() => new AnboxStream(sdkOptions)).toThrow(
+    "update interval 5ms is less than the minimum of 10ms",
+  );
+});
+
+test("should not send sensor data to Anbox container if all sensors are disabled", () => {
+  sdkOptions.devices = {
+    sensor: {
+      enableOrientation: false,
+      enableGyroscope: false,
+      enableAccelerometer: false,
+    },
+  };
+  let { stream } = setupStream(sdkOptions);
+  expect(stream._sensorManager).toBeNull();
+});
+
+test("should send sensor accelerometer data to Anbox container if the sensor is enabled", () => {
+  sdkOptions.devices = {
+    sensor: {
+      enableAccelerometer: true,
+    },
+  };
+  let { stream, snapshots } = setupStream(sdkOptions);
+  expect(stream._sensorManager).not.toBeNull();
+  stream._webrtcReady(null, null);
+
+  window.dispatchEvent(
+    new DeviceMotionEvent("devicemotion", {
+      accelerationIncludingGravity: { x: 1.0, y: 2.5, z: 3.0 },
+    }),
+  );
+
+  expect(snapshots.length).toEqual(1);
+  expect(snapshots[0].type).toEqual("sensor:event");
+  expect(snapshots[0].data).toEqual({
+    sensor: "acceleration",
+    x: 1.0,
+    y: 2.5,
+    z: 3.0,
+  });
+  // gyroscope or orientation data should be availabe in the payload as both are disabled.
+  expect(snapshots[0].type).not.toHaveProperty("gyroscope");
+  expect(snapshots[0].type).not.toHaveProperty("orientation");
+});
+
+test("should send sensor orientation data to Anbox container if the sensor is enabled", () => {
+  sdkOptions.devices = {
+    sensor: {
+      enableOrientation: true,
+    },
+  };
+  let { stream, snapshots } = setupStream(sdkOptions);
+  expect(stream._sensorManager).not.toBeNull();
+  stream._webrtcReady(null, null);
+
+  window.dispatchEvent(
+    new DeviceOrientationEvent("deviceorientation", {
+      alpha: 30.0,
+      beta: 12.0,
+      gamma: 10.0,
+    }),
+  );
+
+  expect(snapshots.length).toEqual(1);
+  expect(snapshots[0].type).toEqual("sensor:event");
+  expect(snapshots[0].data).toEqual({
+    sensor: "orientation",
+    roll: 12.0,
+    pitch: 10.0,
+    azimuth: 30.0,
+  });
+  expect(snapshots[0].type).not.toHaveProperty("gyroscope");
+  expect(snapshots[0].type).not.toHaveProperty("acceleration");
+});
+
+test("should send sensor gyroscope data to Anbox container if the sensor is enabled", () => {
+  sdkOptions.devices = {
+    sensor: {
+      enableGyroscope: true,
+    },
+  };
+  let { stream, snapshots } = setupStream(sdkOptions);
+  expect(stream._sensorManager).not.toBeNull();
+  stream._webrtcReady(null, null);
+
+  window.dispatchEvent(
+    new DeviceMotionEvent("devicemotion", {
+      rotationRate: {
+        alpha: 8,
+        beta: 7,
+        gamma: 6,
+      },
+    }),
+  );
+
+  expect(snapshots.length).toEqual(1);
+  expect(snapshots[0].type).toEqual("sensor:event");
+  expect(snapshots[0].data).toEqual({
+    sensor: "gyroscope",
+    x: 7.0,
+    y: 6.0,
+    z: 8.0,
+  });
+  expect(snapshots[0].type).not.toHaveProperty("orientation");
+  expect(snapshots[0].type).not.toHaveProperty("acceleration");
+});
+
+test("should not send sensor data within the update interval even if sensor data has changed", () => {
+  sdkOptions.devices = {
+    sensor: {
+      enableGyroscope: true,
+      updateInterval: 1000,
+    },
+  };
+  let { stream, snapshots } = setupStream(sdkOptions);
+  expect(stream._sensorManager).not.toBeNull();
+  stream._webrtcReady(null, null);
+
+  window.dispatchEvent(
+    new DeviceMotionEvent("devicemotion", {
+      rotationRate: {
+        alpha: 8.0,
+        beta: 7.0,
+        gamma: 6.0,
+      },
+    }),
+  );
+
+  const ensure_signal_update = () => {
+    expect(snapshots.length).toEqual(1);
+    expect(snapshots[0].type).toEqual("sensor:event");
+    expect(snapshots[0].data).toEqual({
+      sensor: "gyroscope",
+      x: 7.0,
+      y: 6.0,
+      z: 8.0,
+    });
+    expect(snapshots[0].type).not.toHaveProperty("orientation");
+    expect(snapshots[0].type).not.toHaveProperty("acceleration");
+  };
+
+  ensure_signal_update();
+
+  window.dispatchEvent(
+    new DeviceMotionEvent("devicemotion", {
+      rotationRate: {
+        alpha: 10.0,
+        beta: 10.0,
+        gamma: 10.0,
+      },
+    }),
+  );
+
+  ensure_signal_update();
+});
+
+test("should send sensor data to Anbox container if all sensors are enabled", () => {
+  sdkOptions.devices = {
+    sensor: {
+      enableGyroscope: true,
+      enableAccelerometer: true,
+      enableOrientation: true,
+    },
+  };
+  let { stream, snapshots } = setupStream(sdkOptions);
+  expect(stream._sensorManager).not.toBeNull();
+  stream._webrtcReady(null, null);
+
+  window.dispatchEvent(
+    new DeviceMotionEvent("devicemotion", {
+      accelerationIncludingGravity: { x: 1.0, y: 2.5, z: 3.0 },
+      rotationRate: {
+        alpha: 8.0,
+        beta: 7.0,
+        gamma: 6.0,
+      },
+    }),
+  );
+
+  expect(snapshots.length).toEqual(3);
+  expect(snapshots[0].type).toEqual("sensor:event");
+  expect(snapshots[0].data).toEqual({
+    sensor: "orientation",
+    roll: 0.0,
+    pitch: 0.0,
+    azimuth: 0.0,
+  });
+
+  expect(snapshots[1].type).toEqual("sensor:event");
+  expect(snapshots[1].data).toEqual({
+    sensor: "acceleration",
+    x: 1.0,
+    y: 2.5,
+    z: 3.0,
+  });
+
+  expect(snapshots[2].type).toEqual("sensor:event");
+  expect(snapshots[2].data).toEqual({
+    sensor: "gyroscope",
+    x: 7.0,
+    y: 6.0,
+    z: 8.0,
+  });
+
+  // Mock the current time
+  const realDateNow = Date.now.bind(global.Date);
+  let fakeTime = realDateNow();
+  jest.spyOn(global.Date, "now").mockImplementation(() => fakeTime);
+
+  // Advance 1 second
+  fakeTime += 1000;
+
+  window.dispatchEvent(
+    new DeviceOrientationEvent("deviceorientation", {
+      alpha: 30.0,
+      beta: 12.0,
+      gamma: 10.0,
+    }),
+  );
+
+  expect(snapshots.length).toEqual(6);
+  expect(snapshots[3].type).toEqual("sensor:event");
+  expect(snapshots[3].data).toEqual({
+    sensor: "orientation",
+    azimuth: 30,
+    pitch: 10,
+    roll: 12,
+  });
+
+  expect(snapshots[4].type).toEqual("sensor:event");
+  expect(snapshots[4].data).toEqual({
+    sensor: "acceleration",
+    x: 1.0,
+    y: 2.5,
+    z: 3.0,
+  });
+
+  expect(snapshots[5].type).toEqual("sensor:event");
+  expect(snapshots[5].data).toEqual({
+    sensor: "gyroscope",
+    x: 7,
+    y: 6,
+    z: 8,
+  });
+
+  global.Date.now.mockRestore();
+});
